55329c313fa0704fb52a1338a26c261739284762
vanshjangra <vanshjangra586@gmail.com>
2025-09-12 10:46:43 +0530

    working with jdbc template object | practical session

--- src/main/java/com/lcwd/todo/TodoManagerApplication.java
+++ src/main/java/com/lcwd/todo/TodoManagerApplication.java
@@ -1,13 +1,30 @@
 package com.lcwd.todo;
 
+import com.lcwd.todo.dao.TodoDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class TodoManagerApplication {
+public class TodoManagerApplication implements CommandLineRunner {
+    @Autowired
+    private TodoDao todoDao;
+
+    Logger logger = LoggerFactory.getLogger(TodoManagerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoManagerApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+//        System.out.println("Application started:");
+//        JdbcTemplate template = todoDao.getTemplate();
+//        logger.info("Template object : {}", template);
+//        logger.info("Template object : {}", template.getDataSource());
+    }
 }
---
+++ src/main/java/com/lcwd/todo/dao/TodoDao.java
@@ -0,0 +1,19 @@
+package com.lcwd.todo.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TodoDao {
+    @Autowired
+    private JdbcTemplate template;
+
+    public JdbcTemplate getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(JdbcTemplate template) {
+        this.template = template;
+    }
+}
