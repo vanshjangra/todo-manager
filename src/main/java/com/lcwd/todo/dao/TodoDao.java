b95760e4bd8cdd85dd413961f378c53ae099bf8f
vanshjangra <vanshjangra586@gmail.com>
2025-09-12 11:30:58 +0530

    create todo using spring jdbc

--- src/main/java/com/lcwd/todo/dao/TodoDao.java
+++ src/main/java/com/lcwd/todo/dao/TodoDao.java
@@ -1,13 +1,23 @@
 package com.lcwd.todo.dao;
 
+import com.lcwd.todo.models.Todo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TodoDao {
-    @Autowired
     private JdbcTemplate template;
+
+    Logger logger = LoggerFactory.getLogger(TodoDao.class);
+
+    public TodoDao(@Autowired JdbcTemplate template) {
+        this.template = template;
+
+        String createTable = "";
+    }
 
     public JdbcTemplate getTemplate() {
         return template;
@@ -15,5 +25,15 @@ public class TodoDao {
 
     public void setTemplate(JdbcTemplate template) {
         this.template = template;
+    }
+
+    public Todo saveTodo(Todo todo){
+        String insertQuery = "insert into todos(id, title, content, status, addedDate, toDoDate) values(?, ?, ?, ?, ?, ?)";
+
+        int rows = template.update(insertQuery, todo.getId(), todo.getTitle(), todo.getContent(), todo.getStatus(), todo.getAddedDate(), todo.getToDoDate());
+
+        logger.info("JDBC OPERATION: {} inserted", rows);
+
+        return todo;
     }
 }
