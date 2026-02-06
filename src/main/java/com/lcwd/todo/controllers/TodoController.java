b2ce5b630cf90587ebcaf563ed38959eb0487941
vanshjangra <vanshjangra586@gmail.com>
2026-02-06 20:56:10 +0530

    handling todo created response: todo manager

--- src/main/java/com/lcwd/todo/controllers/TodoController.java
+++ src/main/java/com/lcwd/todo/controllers/TodoController.java
@@ -5,6 +5,8 @@ import com.lcwd.todo.services.TodoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +25,11 @@ public class TodoController {
     Random random = new Random();
 
     @PostMapping
-    public Todo createTodoHandler(@RequestBody Todo todo){
+    public ResponseEntity<Todo> createTodoHandler(@RequestBody Todo todo){
         int id = random.nextInt(9999999);
         todo.setId(id);
         logger.info("Create Todo");
         Todo todo1 = todoService.createTodo(todo);
-        return todo1;
+        return new ResponseEntity<>(todo1, HttpStatus.CREATED);
     }
 }
