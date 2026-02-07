1231c262936c649c1d9501ac992573c3e317e916
vanshjangra <vanshjangra586@gmail.com>
2026-02-07 19:56:16 +0530

    sending images in api response

--- src/main/java/com/lcwd/todo/controllers/FileController.java
+++ src/main/java/com/lcwd/todo/controllers/FileController.java
@@ -2,12 +2,14 @@ package com.lcwd.todo.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Arrays;
 
 @RestController
@@ -34,5 +36,17 @@ public class FileController {
         });
 
         return "Handling multiple files";
+    }
+
+    @GetMapping("/serve-image")
+    public void serveImageHandler(HttpServletResponse response){
+        try {
+            InputStream fileInputStream = new FileInputStream("images/download.png");
+            response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+            StreamUtils.copy(fileInputStream, response.getOutputStream());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
